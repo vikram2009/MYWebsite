0b99da5deeb75ef4e7c7d0b9dfaf23541526aa39
vikram2009 <73585468+vikram2009@users.noreply.github.com>
2022-05-24 19:21:41 +0530

    v7
    New Stable version

---
+++ links.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function pigGame(){
+    window.open('https://pig-game-fawn.vercel.app/' , '_blank');
+    
+}
+
+function GMyNum(){
+    window.open('https://guess-my-number-rouge.vercel.app/' , '_blank')
+}
+
+function github(){
+    window.open('https://github.com/vikram2009' , '_blank')
+}
+
+function gitlab(){
+    window.open('https://gitlab.com/vikramksm2009' , '_blank')
+}
+
+function one(){
+    window.open('https://ginyos.blogspot.com' , '_blank')
+}
+
+function two(){
+    window.open('https://tinyurl.com/ginyosbot' , '_blank')
+}
+
+function three(){
+    window.open('https://tinyurl.com/ginyos' , '_blank')
+}
+
+function four(){
+    window.open('https://vikramsrinivas.tech' , '_blank')
+}
+
+function five(){
+    window.open('http://code-with-ginyos.000webhostapp.com/' , '_blank')
+}
+
+function ginyos(){
+    window.open('http://ginyos.tk' , "_blank")
+}
+
+function six(){
+    window.open('https://vikramsrinivas.me ' , '_blank')
+}
+
+function greet(){
+    var name = document.querySelector('.name').value;
+    var txt = document.querySelector('.showtxt');
+    txt.textContent=`Welcome @${name}`;
+
+}
