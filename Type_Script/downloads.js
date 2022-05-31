a1360b123f9b5ad25bd069a453fda9e8e5c1db25
vikram2009 <73585468+vikram2009@users.noreply.github.com>
2022-05-31 14:37:21 +0530

    v7

---
+++ Type_Script/downloads.js
@@ -0,0 +1,13 @@
+'use strict';
+function lib1() {
+    window.open('Src_Downloads\\lib1.h', '_blank');
+}
+function lib2() {
+    window.open('Src_Downloads\\lib2.h', '_blank');
+}
+function piggame() {
+    window.open('https://github.com/vikram2009/PigGame/', '_blank');
+}
+function guessmynumber() {
+    window.open('https://github.com/vikram2009/GUESS-MY-NUMBER/', '_blank');
+}
