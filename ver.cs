322b70bbb392b33dba36bdb437d8eae19f65749f
Vikram Srinivas <73585468+vikram2009@users.noreply.github.com>
2022-06-17 17:23:13 +0530

    Create ver.cs

---
+++ ver.cs
@@ -0,0 +1,19 @@
+namespace Std{
+class Ginyos {
+
+
+static void Main(string[] args ) {
+  
+      
+      string ver  = "7";
+      int ver  = 7 ; 
+      
+      
+      }
+
+
+
+                  }
+  
+ 
+                                    }
