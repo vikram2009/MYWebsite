b9d81b18732bd942489f5d31c496b1b8cc586d09
vikram2009 <vikramksm@hotmail.com>
2022-06-26 10:35:17 +0530

    v7

---
+++ Blog/one/pages/about.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+const about = () => {
+  return (
+
+<div>
+<h1>
+<Link href="/Blog-1.txt">
+    First Blog
+</Link>
+<br/>
+<a href="/Blog-1.txt">
+    FIRST BLOG
+    </a>
+
+     </h1>
+
+    </div>
+  )
+}
+
+export default about
